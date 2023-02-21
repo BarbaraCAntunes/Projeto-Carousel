3ba6c9791debb814b1dbeffd7c0c921ce36209a9
Barbara <barbaracantunes@gmail.com>
2023-02-21 03:06:38 -0300

    Adiciona projeto Carousel

---
+++ src/components/galeria/galeria.component.ts
@@ -0,0 +1,46 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-galeria',
+  standalone: true, 
+  imports: [NgbCarouselModule, NgFor, FormsModule],
+  templateUrl: './galeria.component.html',
+  styleUrls: ["./galeria.component.css"],
+})
+export class GaleriaComponent {
+   images = ["../../assets/1.png", "../../assets/2.png", "../../assets/3.png", "../../assets/4.png"];
+  
+    paused = false;
+    unpauseOnArrow = false;
+    pauseOnIndicator = false;
+    pauseOnHover = true;
+    pauseOnFocus = true;
+  
+    @ViewChild('carousel', { static: true })
+  carousel!: NgbCarousel;
+  
+    togglePaused() {
+      if (this.paused) {
+        this.carousel.cycle();
+      } else {
+        this.carousel.pause();
+      }
+      this.paused = !this.paused;
+    }
+  
+    onSlide(slideEvent: NgbSlideEvent) {
+      if (
+        this.unpauseOnArrow &&
+        slideEvent.paused &&
+        (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+      ) {
+        this.togglePaused();
+      }
+      if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+        this.togglePaused();
+      }
+    }
+  }
